fb6b9928d0af89e79e143c99bf6247c8f5120b89
Lucas Oliveira <d2022004860@unifei.edu.br>
2024-06-30 00:15:07 -0300

    [FEAT] - join

--- backend/gen_exl.py
+++ backend/gen_exl.py
@@ -1,30 +1,30 @@
 import xlsxwriter
 import datetime
-
+from utils import columns_name
 
 def generate_excel(result, output):
     # escrever os nomes das colunas
 
     workbook = xlsxwriter.Workbook(output)
     worksheet = workbook.add_worksheet()
-    colunas = result.all()[0].__table__.columns.values()
+    colunas = columns_name(result.first())
     consulta = result.all()
 
     date_format = workbook.add_format({"num_format": "dd/mm/yyyy"})
     bold_format = workbook.add_format({"bold": True})
     for i, coluna in enumerate(colunas):
-        worksheet.write(0, i, coluna.name, bold_format)
+        worksheet.write(0, i, coluna, bold_format)
 
     coluna = 0
     linha = 1
     for model in consulta:
-        for key in colunas:
-            if type(model.__dict__.get(key.name)) == datetime.date:
+        for i in model:
+            if type(i) == datetime.date:
                 worksheet.write(
-                    linha, coluna, model.__dict__.get(key.name), date_format
+                    linha, coluna, i, date_format
                 )
             else:
-                worksheet.write(linha, coluna, model.__dict__.get(key.name))
+                worksheet.write(linha, coluna, i)
             coluna += 1
         coluna = 0
         linha += 1
--- backend/utils.py
+++ backend/utils.py
@@ -1,3 +1,6 @@
+from sqlalchemy.sql import annotation
+
+
 def calcular_quantidade_de_linhas(inicio, fim):
     if inicio > fim:
         raise ValueError("O início não pode ser maior que o fim")
@@ -5,3 +8,16 @@ def calcular_quantidade_de_linhas(inicio, fim):
         raise ValueError("O intervalo não pode ser maior que 100")
     if inicio < 0 or fim < 0:
         raise ValueError("O início ou o fim não pode ser negativo")
+
+
+def column_name(item):
+    columns = [i for i in item._key_to_index if type(i) == annotation.AnnotatedColumn]
+    for column in columns:
+        yield f"{column.table.name}.{column.name}"
+
+def columns_name(item):
+    columns = [i for i in item._key_to_index if type(i) == annotation.AnnotatedColumn]
+    list_names = []
+    for column in columns:
+        list_names.append(f"{column.table.name}.{column.name}")
+    return list_names
