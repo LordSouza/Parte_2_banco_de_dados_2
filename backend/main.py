21d9b28413f933a3604c39c2891df2415e80fb62
Daniel <danigon1998@gmail.com>
2024-06-30 21:34:49 -0300

    Integração, parte 1

--- backend/main.py
+++ backend/main.py
@@ -14,8 +14,21 @@ import uvicorn
 from utils import calcular_quantidade_de_linhas, column_name
 from schemas import Request_Body
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Response:
